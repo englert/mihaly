d5a609f8ac273763011b6cb420f5c688f8a83ad8
Englert Ervin  Brunó <englert@users.noreply.github.com>
2023-12-13 12:56:53 +0000

    x

--- Hello.py
+++ Hello.py
@@ -1,51 +1,66 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+import sqlite3
+import streamlit as st 
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+def sql_letrehoz():
+    conn = sqlite3.connect("vevok.db")
+    c = conn.cursor()
+    sql = '''
+      SELECT name
+      FROM sqlite_master
+      WHERE type = 'table' AND name = 'vevok'
+      '''
+    c.execute(sql)
+    if not c.fetchone():
+        sql = '''
+        CREATE TABLE vevok (nev text, cim text , telefon text)
+        '''
+        c.execute(sql)
+    conn.commit()
+    conn.close()
 
-    st.write("# Welcome to Streamlit! 👋")
+def sql_megtekint():
+    conn = sqlite3.connect("vevok.db")
+    c = conn.cursor()
+    sql = '''
+    SELECT * FROM vevok
+    '''
+    c.execute(sql)
+    vevok = c.fetchall()
+    conn.commit()
+    conn.close()
+    return vevok
 
-    st.sidebar.success("Select a demo above.")
+def sql_hozzaad(nev,cim,telefon):
+    conn = sqlite3.connect("vevok.db")
+    c = conn.cursor()
+    sql = '''
+    INSERT INTO vevok VALUES(?,?,?)
+    '''
+    c.execute(sql,(nev,cim,telefon))
+    conn.commit()
+    conn.close()
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+sql_letrehoz()
 
+st.title("Vevők")
+malac = st.number_input('malac')
+nev = st.text_input("Név")
+cim = st.text_input("Cím")
+telefon = st.text_input("Telefon szám")
 
-if __name__ == "__main__":
-    run()
+if st.button("hozzáad"):
+    sql_hozzaad(nev,cim,telefon)
+
+if st.button("töröl"):
+    ...
+if st.button("aktualizál"):
+    ...
+if st.button("keres"):
+    ...
+if st.button("megjelenít"):
+    
+    table = sql_megtekint()
+    st.header("Vevők") 
+    st.write(table)
+
